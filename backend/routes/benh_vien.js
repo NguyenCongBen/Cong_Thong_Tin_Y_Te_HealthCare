c16685b687e120bb343ad80956a5d51bb551d9d4
Trương Công Khang <khangtcs32707@fpt.edu.vn>
2024-10-27 19:35:55 +0700

    API

--- backend/routes/benh_vien.js
+++ backend/routes/benh_vien.js
@@ -2,12 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 // Lấy tất cả bệnh viện
-router.get('/thong-tin-benh-vien', (req, res) => {
-    req.db.query(`SELECT * FROM ThongTinBenhVien`, (error, results) => {
+router.get('/', (req, res) => {
+    req.db.query(`SELECT * FROM thong_tin_benh_vien`, (error, results) => {
         if (error) return res.status(500).json({ error: error.message });
         res.json(results);
     });
 });
+router.get('/:id', (req, res) => {
+    const hospitalId = req.params.id;
+    
+    const query = 'SELECT * FROM thong_tin_benh_vien WHERE id = ?';
+    req.db.query(query, [hospitalId], (error, results) => {
+        if (error) {
+            console.error('Database query error:', error);
+            return res.status(500).json({ error: 'An error occurred while fetching data.' });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ message: `No information found for hospital with ID ${hospitalId}.` });
+        }
+
+        res.status(200).json(results[0]);
+    });
+});
+
 
 // Thêm bệnh viện mới
 router.post('/thong-tin-benh-vien', (req, res) => {
