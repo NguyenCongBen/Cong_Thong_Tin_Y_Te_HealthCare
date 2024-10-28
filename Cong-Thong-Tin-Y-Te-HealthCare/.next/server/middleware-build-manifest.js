efa4f30b48b281933de02424b1ee82b1c2a1d143
nguyentrungnhin <nhin12a3@gmail.com>
2024-10-28 14:54:04 +0700

    fetch trung nhịn

--- Cong-Thong-Tin-Y-Te-HealthCare/.next/server/middleware-build-manifest.js
+++ Cong-Thong-Tin-Y-Te-HealthCare/.next/server/middleware-build-manifest.js
@@ -2,7 +2,6 @@ self.__BUILD_MANIFEST = {
   "polyfillFiles": [
     "static/chunks/polyfills.js"
   ],
-<<<<<<< HEAD
   "devFiles": [
     "static/chunks/fallback/react-refresh.js"
   ],
@@ -10,14 +9,9 @@ self.__BUILD_MANIFEST = {
     "static/chunks/fallback/webpack.js",
     "static/chunks/fallback/amp.js"
   ],
-=======
-  "devFiles": [],
-  "ampDevFiles": [],
->>>>>>> 981eaa115292d6fb3829168d082d29943ffce1d4
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
-<<<<<<< HEAD
     "/_app": [
       "static/chunks/fallback/webpack.js",
       "static/chunks/fallback/main.js",
@@ -28,9 +22,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/fallback/main.js",
       "static/chunks/fallback/pages/_error.js"
     ]
-=======
-    "/_app": []
->>>>>>> 981eaa115292d6fb3829168d082d29943ffce1d4
   },
   "ampFirstPages": []
 };
