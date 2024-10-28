e7412e6557a3979ffb8e4b318e05ad882753f70a
nguyentrungnhin <nhin12a3@gmail.com>
2024-10-28 08:10:26 +0700

    Clear cache and ignore .next directory

--- Cong-Thong-Tin-Y-Te-HealthCare/src/app/(users)/layout.js
+++ Cong-Thong-Tin-Y-Te-HealthCare/src/app/(users)/layout.js
@@ -5,7 +5,7 @@ import "../../../public/css/style.css";
 import "../../../public/bootstrap/css/bootstrap.min.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Providers from "../../../redux/Provider";
+// import Providers from "../../../redux/Provider";
 
 export const metadata = {
   title: "Create Next App",
@@ -19,11 +19,11 @@ export default function RootLayout({ children }) {
         <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
-        <Providers>
-          <Header />
-          {children}
-          <Footer />
-        </Providers>
+        {/* <Providers> */}
+        <Header />
+        {children}
+        <Footer />
+        {/* </Providers> */}
 
         <script src="https://kit.fontawesome.com/2b5ed98ab2.js" crossorigin="anonymous"></script>
         <script src="/bootstrap/js/style.js"></script>
